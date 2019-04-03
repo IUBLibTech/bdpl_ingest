c87c938fec39707300f64cedee33884b91dbe65c
shallcro <43279347+shallcro@users.noreply.github.com>
2019-04-03 17:36:41 -0400

    Update BDPL_launch.bat

--- BDPL_launch.bat
+++ BDPL_launch.bat
@@ -1,21 +1,13 @@
 @ECHO OFF
 
-if "%2" == "" (
-    ECHO Add local address of files
-)   else (
-    ECHO %1
-)
+if "%1." =="." GOTO No1
+if "%2." =="."  GOTO No2
 
 TYPE %2\scripts\bdpl.txt
 ECHO.
 ECHO.
 ECHO.
 
-if "%1" == "" (
-    ECHO Add server address as argument
-)   else (
-    ECHO %1
-)
 
 IF NOT EXIST Z: (
   REM Get username
@@ -32,5 +24,12 @@ TYPE %2\scripts\bdpl.txt
 python %2\scripts\bdpl_ingest.py
 
 
+:No1
+ECHO Missing local directory argument
+ECHO.
+PAUSE
 
-
+:No2
+ECHO Missing server address
+ECHO.
+PAUSE
