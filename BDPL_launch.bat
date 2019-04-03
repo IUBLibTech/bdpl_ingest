57fa63f1c66dce7664839e75a8fb323844d84288
shallcro <43279347+shallcro@users.noreply.github.com>
2019-04-03 17:23:47 -0400

    Add files via upload

---
+++ BDPL_launch.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+TYPE C:\BDPL\scripts\bdpl.txt
+ECHO.
+ECHO.
+ECHO.
+
+if "%1" == "" (
+    ECHO Add server as argument
+)   else (
+    ECHO %1
+)
+
+IF NOT EXIST Z: (
+  REM Get username
+  SET /P _username="Enter your IU username: "
+
+  REM Server passed in as CMD.EXE arg
+  
+  REM Connect to shared drive
+  NET USE Z: %1 /user:ads\%_username% *
+)
+
+CLS
+TYPE C:\BDPL\scripts\bdpl.txt
+python C:\BDPL\scripts\bdpl_ingest.py
+
+
+
+
