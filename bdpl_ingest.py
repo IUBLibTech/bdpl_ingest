3feab6040250844a7a053211a04fad81d7c196f5
shallcro <43279347+shallcro@users.noreply.github.com>
2019-04-05 10:27:31 -0400

    Update bdpl_ingest.py

--- bdpl_ingest.py
+++ bdpl_ingest.py
@@ -1272,7 +1272,7 @@ def print_premis(premis_path):
 
     PREMIS = "{%s}" % PREMIS_NAMESPACE
 
-    NSMAP = {'PREMIS' : PREMIS_NAMESPACE,
+    NSMAP = {'premis' : PREMIS_NAMESPACE,
             "xsi": "http://www.w3.org/2001/XMLSchema-instance",
             "xlink": "http://www.w3.org/1999/xlink"}
 
