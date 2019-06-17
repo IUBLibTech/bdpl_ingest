8916881daf51dcc661aa5ae30e1ac20e5aaa907b
U-ADS\micshall <micshall@bl-libg-ltdplib.ads.iu.edu>
2019-06-17 17:18:09 -0400

    Refine SIP packaging workflow

--- bdpl_bag-prep.py
+++ bdpl_bag-prep.py
@@ -42,8 +42,8 @@ def main():
     wb = openpyxl.load_workbook(spreadsheet)
     ws = wb['Appraisal']
     
-    #make sure that the spreadsheet is correctly structured; if there are any errors, exit.
-    if ws['AA1'].value != 'Appraisal results' or ws['G1'].value != 'Source type' or ws['H1'].value != 'Label transcription' or ws['I1'].value != 'Initial appraisal notes':
+    #make sure column headings are correct; exit if they don't match targets
+    if ['Appraisal results', 'Source type', 'Label transcription', 'Initial appraisal notes'] != [ws['AA1'].value, ws['G1'].value, ws['H1'].value, ws['I1'].value]:
         print('\n\nERROR: SPREADSHEET COLUMNS ARE NOT IN CORRECT ORDER')
         print('Current headings:\n -AA1 (Appraisal results) = %s\n - G1 (Source type) = %s\n - H1 (Label transcription) = %s\n - I1 (Initial appraisal notes) = %s' % (ws['AA1'].value, ws['G1'].value, ws['H1'].value, ws['I1'].value))
         sys.exit(1)
@@ -63,15 +63,34 @@ def main():
     #set shipment directory as current working directory
     os.chdir(shipment)
     
-    #create lists to track work (NOTE: probably a better way to do this...)
+    
+    #determine if directory includes folders not on spreadsheet and/or if spreadsheet has barcodes not in directory
+    spreadsheet_list = []
+    for col in ws['A']:
+        if col.value == 'Identifier':
+            continue
+        else:
+            spreadsheet_list.append(col.value)
+    
+    dir_list = next(os.walk(shipment))[1]
+
+    missing_from_dir = list(set(spreadsheet_list) - set(dir_list))
+    missing_from_spreadsheet = list(set(spreadsheet_list) - set(dir_list))
+    
+    #save the 'missing' lists to files, for future reference
+    unaccounted_folders = os.path.join(shipment, 'unaccounted.txt') #for barcodes that are in directory, but not in spreadsheet; need to check for data entry errors
+    with open(unaccounted_folders, 'w') as f:
+        f.write('\n'.join(missing_from_spreadsheet))
+    
+    #create lists to track work (NOTE: there is probably a better way to do this...)
     started_list = os.path.join(shipment, 'started.txt') #to document all the barcodes that we started to package/process; all should be accounted for in the fail/complete lists
-    skipped_list = os.path.join(shipment, 'skipped.txt')    #for barcodes/items that will not be sent to SDA
+    deaccession_list = os.path.join(shipment, 'deaccession.txt')    #for barcodes/items that will not be sent to SDA
     failed_list = os.path.join(shipment, 'failed-packaging.txt') #for any failures; will note which stage the failure occurred 
     bagged_list = os.path.join(shipment, 'bagged.txt') #for barcodes that were successfully bagged
     tarred_list = os.path.join(shipment, 'tarred.txt') #for barcodes that were successfully tarred
     cleaned_list = os.path.join(shipment, 'cleaned.txt') #for barcode folders that were successfully cleaned
     other_list = os.path.join(shipment, 'other-decision.txt') #for barcode folders have an alternate appraisal decision
-    completed_list = os.path.join(shipment, 'completed.txt') #for barcodes that reached the end of the process; should include any that were skipped
+    completed_list = os.path.join(shipment, 'completed.txt') #for barcodes that reached the end of the process; should include any that were deaccessioned
 
     #loop through spreadsheet, skipping header row
     iterrows = ws.iter_rows()
@@ -81,9 +100,9 @@ def main():
 
         barcode = str(row[0].value)
         
-        #description for bag-info.txt currently includes source media, label transcription, and appraisal note.
-        desc = '%s. %s. %s' %(str(row[6].value), str(row[7].value), str(row[8].value))
-        desc.replace('\n', ' ')
+        #skip to next barcode if current one does not have a folder in the shipment direcetory
+        if barcode in missing_from_dir:
+            continue
         
         #skip to next barcode if current one has already finished workflow
         if check_list(completed_list, barcode):
@@ -94,18 +113,29 @@ def main():
             list_write(started_list, barcode)
         
         print('\nWorking on item: %s' % barcode)
+
+        #description for bag-info.txt currently includes source media, label transcription, and appraisal note.
+        desc = '%s. %s. %s' %(str(row[6].value), str(row[7].value), str(row[8].value))
+        desc.replace('\n', ' ')        
         
         #if content will not be moved to SDA, just skip folder for now and write to skipped and completed lists
         if str(row[26].value) == "Delete content":
-            list_write(skipped_list, barcode)
+            list_write(deaccession_list, barcode)
             print('\n\tContent will not be transferred to SDA.  Continuing with next item.')
             continue
         
         #if content has been determined to be of value, complete prep workflow.
         elif str(row[26].value) == "Transfer to SDA":
             
-            #get full path to barcode folder
+            '''CHECK THAT FOLDER EXISTS'''
+            #get full path to barcode folder.
             target = os.path.join(shipment, barcode)
+            
+            #make sure folder exists; note failure if missing
+            if not os.path.exists(target):
+                print('\n\tBarcode folder does not exist!')
+                list_write(failed_list, barcode, 'check_folder\tFOLDER DOES NOT EXIST')
+                continue
             
             '''CLEAN FOLDERS'''
             #remove bulk_extractor folder, if present, as well as reports used solely for appraisal/review
@@ -120,7 +150,7 @@ def main():
                 report = os.path.join(target, 'metadata', 'reports', f)
                 if os.path.exists(report):
                     os.remove(report)
-                
+            
             '''BAG FOLDER'''
             #make sure we haven't already bagged folder
             if not check_list(bagged_list, barcode):
@@ -206,7 +236,7 @@ def main():
             
     '''CHECK PROCESS FOR MISSING/FAILED ITEMS'''    
     #get lists from status files
-    list_dict = {'started' : started_list, 'completed' : completed_list, 'skipped' : skipped_list, 'other' : other_list, 'failed' : failed_list}
+    list_dict = {'started' : started_list, 'completed' : completed_list, 'deaccessioned' : deaccession_list, 'other' : other_list, 'failed' : failed_list}
     tally_dict = {}
     for key, value in list_dict.items():
         if os.path.isfile(value):
@@ -219,20 +249,22 @@ def main():
         tally_dict[key] = tally
     
     #determine if any didn't make it through the process to one of the conclusions (skipped, failed, completed, or other)
-    subtotal = tally_dict['completed'] + tally_dict['other'] + tally_dict['failed'] + tally_dict['skipped']
+    subtotal = tally_dict['completed'] + tally_dict['other'] + tally_dict['failed'] + tally_dict['deaccessioned']
     missing = list(set(tally_dict['started']) - set(subtotal))
     
-    print('\n\nSTATS:\n# of barcodes processed:\t\t%s\
-        \n# of barcodes moved to Archiver:\t%s\
-        \n# of barcodes to be deleted:\t\t%s\
-        \n# of barcodes that failed:\t\t%s\
-        \n# of barcodes that require review:\t%s' % (len(tally_dict['started']), len(tally_dict['completed']), len(tally_dict['skipped']), len(tally_dict['failed']), len(tally_dict['other']))) 
+    print('\n\nSTATS:\n# of barcodes processed:\t\t%s' % len(tally_dict['started']))
+    print('# of barcodes moved to Archiver:\t%s' % len(tally_dict['completed']))
+    print('# of barcodes to be deaccessioned:\t%s' % len(tally_dict['deaccessioned']))
+    print('# of barcodes that failed:\t\t%s' % len(tally_dict['failed']))
+    print('# of barcodes that require review:\t%s' % len(tally_dict['other']))
         
     #make sure all items are accounted for
     if len(missing) > 0:
         print("\n# of barcodes not accounted for:\t%s\n\t%s" % (len(missing), '\n\t'.join(missing)))
-    else:
-        print('\n\nAll barcodes have been accounted for.')
+    
+    #give alert if there are barcode folders not listed in spreadsheet
+    if len(missing_from_spreadsheet) > 0:
+        print('\n\nNOTE: shipment folder includes %s barcode folder(s) not listed in spreadsheet.  Review as needed.' % len(missing_from_spreadsheet))
         
 
 if __name__ == '__main__':
